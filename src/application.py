7ee3d422070bbc8e9056fc9268762c8cf7b18758
Rafal Maziejuk <rafalmaziejuk@gmail.com>
2021-12-28 13:54:59 +0100

    Represent OpenGL shader as class for easier usage
    Signed-off-by: Rafal Maziejuk <rafalmaziejuk@gmail.com>
    Related-To: PYRT-3

--- src/application.py
+++ src/application.py
@@ -2,48 +2,17 @@ from window import Window
 from renderer import Renderer
 from vertex_array import VertexArray
 from vertex_buffer import VertexBuffer, ElementBuffer, BufferLayout, Types
+from shader import Shader
 from OpenGL.GL import *
 
-vertexShaderSource = """
-    #version 330 core
-    layout (location = 0) in vec3 aPos;
-    void main()
-    {
-       gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);
-    }
-    """
-
-fragmentShaderSource = """
-    #version 330 core
-    out vec4 FragColor;
-    void main()
-    {
-        FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);
-    }
-    """
-
 class Application():
-    def __init__(self, width=800, height=600, name="PyRayTracer"):
+    def __init__(self, width=800, height=600, name='PyRayTracer'):
         self.window = Window(width, height, name)
 
     def run(self):
-        vertexShader = glCreateShader(GL_VERTEX_SHADER)
-        glShaderSource(vertexShader, vertexShaderSource)
-        glCompileShader(vertexShader)
+        shader = Shader('shaders/shader.vert', 'shaders/shader.frag')
+        shader.bind()
 
-        fragmentShader = glCreateShader(GL_FRAGMENT_SHADER)
-        glShaderSource(fragmentShader, fragmentShaderSource)
-        glCompileShader(fragmentShader)
-
-        shaderProgram = glCreateProgram()
-        glAttachShader(shaderProgram, vertexShader)
-        glAttachShader(shaderProgram, fragmentShader)
-        glLinkProgram(shaderProgram)
-        
-        glDeleteShader(vertexShader);
-        glDeleteShader(fragmentShader);
-        glUseProgram(shaderProgram)
-        
         vertices = [
              0.5,  0.5, 0.0,
              0.5, -0.5, 0.0,
@@ -69,10 +38,10 @@ class Application():
 
             Renderer.clear()
 
-            glUseProgram(shaderProgram);
             glDrawElements(GL_TRIANGLES, vertexArray.elementBuffer.elementCount, GL_UNSIGNED_INT, None)
 
             self.window.update()
 
+        shader.cleanup()
         vertexArray.cleanup()
         self.window.cleanup()
